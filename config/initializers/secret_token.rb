1c6d3531f9a12f89236fde2944119ebcc2a751fb
allansideas <allansideas@gmail.com>
2012-11-09 11:41:37 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thperiment::Application.config.secret_token = '179c05e38b71b36e8ed1175011fc6f9b27f4830b7880e4d83c038046bddb39abd34affc5d1c113a67e881a3cfb39fbbd8d5edb2c403446803c279168fbbb3dd4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Thperiment::Application.config.session_store :cookie_store, key: '_thperiment_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Thperiment::Application.config.session_store :active_record_store
